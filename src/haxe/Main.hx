0b5f7861be13514b2cf0ab2ab4362d88ddafdbd4
mrchnk <mrchnk@gmail.com>
2019-01-18 01:24:55 +0300

    refactor whitespaces

--- src/haxe/Main.hx
+++ src/haxe/Main.hx
@@ -1,19 +1,12 @@
-
-
 import openfl.display.Sprite;
 import openfl.Assets;
 
-
 class Main extends Sprite {
 
-
-	public function new () {
-
-		super ();
-
-		var ab = Assets.getMovieClip ("ab:B");
-		addChild (ab);
-
-	}
+  public function new () {
+    super ();
+    var ab = Assets.getMovieClip ("ab:B");
+    addChild (ab);
+  }
 
 }
